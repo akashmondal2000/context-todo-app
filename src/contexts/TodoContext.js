2d23f89a771a02861be39f1d1c204bb0d0abf5ef
akashmondal2000 <mondalakash494@gmail.com>
2024-06-16 00:11:30 +0530

    create Todo Context

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,9 +1,22 @@
-import { createContext , useContext } from "react";
+import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo Massege",
+      completed: false,
+    },
+  ],
+
+  addTodo : (todo)=>{},
+  updateTodo : (id,todo)=>{},
+  deleteTodo : (id)=>{},
+  toggleCompete : (id)=>{}  
+});
 
 export const useTodo = () => {
-    return useContext(TodoContext)
-}
+  return useContext(TodoContext);
+};
 
-export const TodoProvider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider;
