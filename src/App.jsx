c01614c67203ca330be6ee83b9f9a50972a2001b
akashmondal2000 <mondalakash494@gmail.com>
2024-06-17 00:44:26 +0530

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
+import { TodoFrom } from "./components";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -42,7 +43,9 @@ const App = () => {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoFrom/>
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
           </div>
