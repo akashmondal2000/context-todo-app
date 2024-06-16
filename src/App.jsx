99fdd18ec7c8a34f01125972f262e04aabc27c76
akashmondal2000 <mondalakash494@gmail.com>
2024-06-17 00:31:22 +0530

    store todos in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { TodoProvider } from "./contexts";
 
@@ -20,6 +20,18 @@ const App = () => {
  const toggleCompete = (id) =>{
   setTodos ((prev)=> prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo))
   }
+
+  useEffect(()=>{
+   const todos =  JSON.parse(localStorage.getItem("todos")) 
+
+   if (todos && todos.length>0 ) {
+    setTodos(todos)
+   }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
 
   return (
     <TodoProvider
