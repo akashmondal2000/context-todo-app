09f04e1c1c6eb7957f8db3357b339b6411f7cb98
akashmondal2000 <mondalakash494@gmail.com>
2024-06-17 12:09:14 +0530

    create todoFrom component

--- src/components/TodoFrom.jsx
+++ src/components/TodoFrom.jsx
@@ -4,12 +4,27 @@ import { useTodo } from "../contexts";
 const TodoForm = () => {
   const [todo, setTodo] = useState("");
   const {addTodo} = useTodo();
+
+  const add = (e) =>{
+    e.preventDefault();
+
+    if(!todo){
+      return
+    }else{
+      addTodo({todo:todo,completed: false});
+      setTodo("");
+    }
+
+
+  }
   return (
-    <form className="flex">
+    <form className="flex" onSubmit={add}>
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e)=> setTodo(e.target.value)}
       />
       <button
         type="submit"
